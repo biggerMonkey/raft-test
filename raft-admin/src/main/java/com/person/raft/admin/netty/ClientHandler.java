6d0d450a59c24214bc4d8f59b78764167a558eff
huangwenjun@lixin360.com <huangwenjun@lixin360.com>
2018-07-12 21:19:36 +0800

    调整结构

---
+++ raft-admin/src/main/java/com/person/raft/admin/netty/ClientHandler.java
@@ -0,0 +1,46 @@
+package com.person.raft.admin.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.net.InetAddress;
+
+/**
+ * @author huangwenjun
+ * @Date 2018年7月12日
+ */
+public class ClientHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println("client msg:" + msg);
+        String clientIdToLong = ctx.channel().id().asLongText();
+        System.out.println("client long id:" + clientIdToLong);
+        String clientIdToShort = ctx.channel().id().asShortText();
+        System.out.println("client short id:" + clientIdToShort);
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+        System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " active !");
+
+        ctx.channel().writeAndFlush(
+                "Welcome to " + InetAddress.getLocalHost().getHostName() + " service!\n");
+
+        super.channelActive(ctx);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("\nChannel is disconnected");
+        super.channelInactive(ctx);
+    }
+}
